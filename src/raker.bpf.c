e0d475d671e21103d5e28907fe87a0f0f7950b1d
Desmond Cheong Zhi Xi <desmondcheongzx@gmail.com>
2021-06-22 13:47:27 +0800

    xdp: fix double checksum computation

--- src/raker.bpf.c
+++ src/raker.bpf.c
@@ -24,17 +24,23 @@ static __always_inline void ipv4_csum(void *data_start, int data_size,
 static void parse_ipv4(void *data, u64 nh_off, void *data_end)
 {
 	__u32 csum = 0;
+	__u32 csum2 = 0;
 	struct iphdr *iph = data + nh_off;
 
 	if ((void *)(iph + 1) > data_end)
 		return;
 	int ver = iph->version;
+	bpf_printk("xdp: %d\n", ver);
+	bpf_printk("xdp: from %d to %d\n", iph->saddr, iph->daddr);
 	int check = iph->check;
+	bpf_printk("xdp, old checksum: %d\n", check);
 	if (ver == 4) {
+		bpf_printk("xdp: computing checksum\n");
 		iph->check = 0;
-		ipv4_csum(iph, sizeof(struct iphdr), &csum); // Compute checksum twice
+		ipv4_csum(iph, sizeof(struct iphdr), &csum2);
 		ipv4_csum(iph, sizeof(struct iphdr), &csum);
 		check = csum;
+		bpf_printk("xdp, new checksum: %d\n", check);
 		iph->check = csum;
 	}
 }
@@ -48,6 +54,9 @@ int xdp_prog(struct xdp_md *ctx)
 	u64 nh_off;
 
 	nh_off = sizeof(*eth);
+	bpf_printk("xdp, data len: %li\n", ctx->data_end - ctx->data);
+	bpf_printk("xdp, data pos: %li, %li\n", (long)(data + sizeof(short)), (long)(data_end));
+	(data + sizeof(short) > data_end) ? bpf_printk("true\n") : bpf_printk("false\n");
 	if (data + nh_off > data_end)
 		return 0;
 	parse_ipv4(data, nh_off, data_end);
